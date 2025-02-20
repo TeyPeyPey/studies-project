5630ad43a23aebcc812b069893f8c356f842372d
TeyPeyPey <teyonce143@gmail.com>
2025-02-20 00:31:24 -0600

    saving work

--- operators.js
+++ operators.js
@@ -1,6 +1,35 @@
 /**
- * OPERATORS:
+ * OPERATORS: ASSIGNMENT
  * 
  * 0: (introduction)
  * 
+ */
+
+/**
+ * OPERATORS: ARITHMETIC 
+ * 
+ * 
+ */
+
+/**
+ * OPERATORS: COMPARISON 
+ * 
+ * 
+ */
+
+/**
+ * OPERATORS: LOGICAL 
+ * 
+ * 
+ */
+
+/**
+ * OPERATORS: UNARY 
+ * (!, typeOf, -)
+ * 
+ */
+
+/**
+ * OPERATORS: TERNARY
+ * (a ? b : c)
  */
