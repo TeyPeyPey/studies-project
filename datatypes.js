5d78e0556c06678a9a7bad5c323406ddc02966ce
TeyPeyPey <teyonce143@gmail.com>
2025-02-20 00:22:32 -0600

    saving

--- datatypes.js
+++ datatypes.js
@@ -1,6 +1,73 @@
 /**
- * DATATYPES:
+ * DATATYPES: NUMBER 
  * 
  * 0: (introduction)
  * 
+ */
+
+/**
+ * DATATYPES: STRING
+ * 
+ * 
+ * 
+ */
+
+/**
+ * DATATYPES: BOOLEAN
+ * 
+ * 
+ */
+
+/**
+ * DATATYPES: ARRAY 
+ * 
+ * 
+ */
+
+/**
+ * DATATYPES: OBJECT
+ * 
+ * 
+ * 
+ */
+
+/**
+ * DATATYPES: FUNCTION 
+ * 
+ * 
+ */
+
+/**
+ * DATATYPES: undefined
+ * 
+ * 
+ */
+
+/**
+ * DATATYPES: null
+ * 
+ * 
+ */
+
+/**
+ * DATATYPES: NaN
+ * 
+ * 
+ */
+
+/**
+ * DATATYPES: Infinity and -Infinity
+ * 
+ * 
+ */
+
+/**
+ * DATATYPES: Primitive/Simple vs Complex
+ * Primitve/Simple by Copy 
+ * Complex by reference 
+ * 
+ */
+
+/**
+ * DATATYPES: By Copy vs By reference 
  */
